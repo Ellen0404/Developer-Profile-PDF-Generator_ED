555787f9a3ec04e25f4904bb2815f43d658bcf0f
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-02-09 18:12:29 -0700

    creating starting files and package json

---
+++ index.js
@@ -0,0 +1,18 @@
+// library packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateHTML = require("./generateHTML");
+
+ console.log(generateHTML.colors);
+
+// const questions = [
+  
+// ];
+
+// function writeToFile(fileName, data) {
+ 
+// }
+
+// function init() {
+
+// init();
